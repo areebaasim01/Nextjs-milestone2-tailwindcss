6dd6d03b9b1a5ab2255c4130b6b7f037fd2bb848
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-12-11 20:26:06 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
 export default function About() {
   const aboutText = `Hi I am Areeba Ansari, a passionate Web Developer and Sociologist with a unique blend of technical expertise and social insight. I specialize in crafting modern, user-friendly websites using technologies like HTML, CSS, TypeScript, and Next.js. My background in sociology helps me design digital solutions that are not only functional but also inclusive and empathetic, ensuring they cater to diverse audiences and societal needs. With a strong foundation in front-end development, I focus on creating seamless, responsive experiences that engage users and drive results. I'm always exploring new tools and techniques to enhance my skills and stay ahead of the curve in the ever-evolving tech landscape. Whether it’s building dynamic web applications or analyzing user behavior, I’m committed to creating solutions that merge the power of technology with a deep understanding of people.`;
 
